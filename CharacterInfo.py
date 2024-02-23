28d61504ceac2f75a324c4d30ed5cf2709a55bc7
Blongtato <anthonycharleston05@gmail.com>
2024-02-23 10:48:35 -0500

    Added Comments

--- CharacterInfo.py
+++ CharacterInfo.py
@@ -1,4 +1,5 @@
 class Character:
+    # Constructor assigns class attributes
     def __init__(self, height, faction, debut, origin, race, haki, df_type):
         self.height = height
         self.faction = faction
@@ -8,29 +9,29 @@ class Character:
         self.haki = haki
         self.df_type = df_type
 
-
+    # Prints out attributes of Class
     def print_info(self):
         print(self.height, self.faction, self.debut, self.origin, self.race, self.haki, self.df_type)
 
-
+# List holds all story arcs so they can be accessed by index
 arcs = ["Romance Dawn", "Orange Town", "Syrup Village", "Baratie", "Arlong Park", "Loguetown", "Reverse Mountain",
         "Whisky Peak", "Little Garden", "Drum Island", "Alabasta", " Jaya", "Skypiea", "Long Ring Long Land", "Water 7",
         "Enies Lobby", "Post-Enies Lobby", "Thriller Bark", "Sabaody Archipelago", "Amazon Lily", "Impel Down",
         "Marineford", "Post-War", "Return to Sabaody", "Fish-Man Island", "Punk Hazard", "Dressrosa", "Zou",
         "Whole Cake Island", "Levely", "Wano", "Egghead"]
 
-people = {
-    "Carrot": Character(1.61, ["Kingdom", "Zou"], 27, ["New World", "Zou"], "Mink", 0, "None"),
+people = {  # Dictionary populated with all available characters with name as key and instance as value
     "Bepo": Character(2.40, ["Pirate", "Heart Pirates"], 18, ["New World", "Zou"], "Mink", 0, "None"),
     "Boa Hancock": Character(1.91, ["Pirate", "Kuja Pirates"], 19, ["Calm Belt", "Amazon Lily"], "Human", 3, "Paramecia"),
     "Brook": Character(2.77, ["Pirate", "Straw Hat"], 17, ["West Blue", "Unknown"], "Human", 0, "Paramecia"),
+    "Carrot": Character(1.61, ["Kingdom", "Zou"], 27, ["New World", "Zou"], "Mink", 0, "None"),
     "Donquixote Rosinante": Character(2.93, ["World Government", "Marines"], 26, ["Red Line", "Mary Geoise"], "Human", 0, "Paramecia"),
     "Eggplant Soldier": Character(1.35, ["Pirate", "Big Mom Pirates"], 28, ["New World", "Whole Cake Island"], "Human", 0, "None"),
     "Franky": Character(2.40, ["Pirate", "Straw Hat"], 14, ["South Blue", "Unknown"], "Human", 0, "None"),
     "Jinbe": Character(3.01, ["Pirate", "Straw Hat"], 20, ["Grand Line", "Fish-Man Island"], "Fish-Man", 2, "None"),
     "Kozuki Momonosuke": Character(1.10, ["Kingdom", "Wano"], 25, ["Grand Line", "Unknown"], "Human", 1, "Zoan"),
     "Monkey D. Luffy": Character(1.74, ["Pirate", "Straw Hat"], 0, ["East Blue", "Goa Kingdom"], "Human", 3, "Zoan"),
-    "Nami": Character(1.70, ["Pirate", "Straw Hat"], 1, ["East Blue", "Conomi Islands"], "Human", 0, "None"),
+    "Nami": Character(1.70, ["Pirate", "Straw Hat   "], 1, ["East Blue", "Conomi Islands"], "Human", 0, "None"),
     "Nico Robin": Character(1.88, ["Pirate", "Straw Hat"], 7, ["West Blue", "Ohara"], "Human", 0, "Paramecia"),
     "Rob Lucci": Character(2.12, ["World Government", "Cipher Pol"], 14, ["Grand Line", "Unknown"], "Human", 2, "Zoan"),
     "Roronoa Zoro": Character(1.81, ["Pirate", "Straw Hat"], 0, ["East Blue", "Unknown"], "Human", 3, "None"),
@@ -42,4 +43,4 @@ people = {
     "Vinsmoke Sanji": Character(1.80, ["Pirate", "Straw Hat"], 3, ["North Blue", "Germa Kingdom"], "Human", 2, "None"),
     "Yamato": Character(2.63, ["Kingdom", "Wano"], 30, ["New World", "Wano"], "Oni", 3, "Zoan")
 }
-names = list(people.keys())
+names = list(people.keys())  # Turns keys of dictionary into list of names so random generator can pick one
--- Guess.py
+++ Guess.py
@@ -1,50 +1,52 @@
 import CharacterInfo as ci
 import random
-def compare(x, y):
+def compare(x, y):  # Compares character selected by user to actual character by each attribute
+    # Height Comparison
     if x.height > y.height:
         height_var = "Shorter"
     elif x.height < y.height:
         height_var = "Taller"
     else:
         height_var = "Correct!"
-
+    # Faction Comparison
     if x.faction == y.faction:
         fact_var = "Correct!"
     elif x.faction[0] == y.faction[0]:
         fact_var = "Close"
     else:
         fact_var = "Incorrect"
-
+    # Debut Arc Comparison
     if x.debut > y.debut:
         deb_var = "Earlier"
     elif x.debut < y.debut:
         deb_var = "Later"
     else:
         deb_var = "Correct!"
-
+    # Origin Comparison
     if x.origin == y.origin:
         or_var = "Correct!"
     elif x.origin[0] == y.origin[0]:
         or_var = "Close"
     else:
         or_var = "Incorrect"
-
+    # Race Comparison
     if x.race == y.race:
         race_var = "Correct!"
     else:
         race_var = "Incorrect"
-
+    # Haki Comparison
     if x.haki > y.haki:
         haki_var = "Less"
     elif x.haki < y.haki:
         haki_var = "More"
     else:
         haki_var = "Correct!"
-
+    # Devil Fruit Comparison
     if x.df_type == y.df_type:
         df_var = "Correct!"
     else:
         df_var = "Incorrect"
+    # Prints out comparisons
     print(f"|Height: {x.height}m, {height_var}|\n"
           f"|Group: {x.faction[0]}({x.faction[1]}), {fact_var}|\n"
           f"|Debut Arc: {ci.arcs[x.debut]}, {deb_var}|\n"
@@ -53,9 +55,8 @@ def compare(x, y):
           f"|Haki: {x.haki} forms, {haki_var}|\n"
           f"|Devil Fruit: {x.df_type}, {df_var}|")
 
-
+# User inputs character guess and chooses a random character from list
 guess = input("Input first character : ")
-
 answer = random.choice(ci.names)
 
 while guess != answer:
